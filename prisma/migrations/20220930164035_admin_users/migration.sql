12c78816bcbfe7010d6dbb344339dab52b682eb9
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2022-10-01 00:40:59 +0800

    Add table for admin users

---
+++ prisma/migrations/20220930164035_admin_users/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "admins" (
+    "userId" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_userId_key" ON "admins"("userId");
+
+-- AddForeignKey
+ALTER TABLE "admins" ADD CONSTRAINT "admins_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
